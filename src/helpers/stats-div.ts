d8b6545b97d4ac05cac052e1f640f8be5a8f68ad
John Wakley <john.wakley@icloud.com>
2024-02-21 10:13:33 -0500

    created stats-div class

---
+++ src/helpers/stats-div.ts
@@ -0,0 +1,32 @@
+import { Engine } from '../ecs-engine';
+
+export class StatsDiv {
+    docTitle: string;
+    div: HTMLDivElement;
+
+    constructor(docTitle: string = 'ecs-engine-demo') {
+        this.docTitle = docTitle;
+
+        this.div = document.createElement('div');
+        this.div.style.setProperty('position', 'absolute');
+        this.div.style.setProperty('top', '0');
+        this.div.style.setProperty('right', '0');
+        this.div.style.setProperty('color', 'white');
+        this.div.style.setProperty('background-color', 'black');
+        this.div.style.setProperty('padding', '16px');
+        this.div.style.setProperty('font-size', '1.2rem');
+        this.div.style.setProperty('z-index', '100');
+
+        document.title = this.docTitle;
+        document.body.appendChild(this.div);
+    }
+
+    update(engine: Engine): void {
+        this.div.innerHTML = [
+            `Components: ${engine.components.length}`,
+            `Systems: ${engine.systems.length}`,
+            `Entities: ${engine.entities.length}`,
+            `FPS: ${engine.clock.fps}`,
+        ].join('<br>');
+    }
+}
